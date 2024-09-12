025e1890b43759fe271591f6318cba5cdfe312e7
baasanjargal berkhee <baasanjargalberkhee@baasanjargals-MacBook-Air.local>
2024-09-12 16:19:30 +0800

    huwisagchid utga onooh

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ var roundScore = 0;
 
 // shoo buuh bolgondoo ali talaaraa buuhiig hadgalah huwisagch
 
-var  dice = Math.floor(Math.random() * 6) + 1; 
+var  diceNumber = Math.floor(Math.random() * 6) + 1; 
 
-document.querySelector('#score-0').textContent = 0;
+document.getElementById('#score-0').textContent = diceNumber;
+
+// document.querySelector('#score-0').textContent = 0;
 document.querySelector('#score-1').textContent = 0; 
 document.querySelector("#current-0").textContent = 0;
 document.querySelector("#current-1").textContent = 0;
